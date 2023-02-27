04d6828655b53e34cdb69bce21fd2e1e42b758f4
HBA114 <hbasriayhaner114@gmail.com>
2023-02-27 14:20:29 +0300

    Fixed Routes with parameter, Added HttpPatch with required packages
    Fixed route parameters, added JsonPatch and NewtonsoftHson from Microsoft.AspNetCore, created httppatch method and tested.

--- basics/bookDemo/Controllers/BooksController.cs
+++ basics/bookDemo/Controllers/BooksController.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using bookDemo.Data;
 using bookDemo.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bookDemo.Controllers;
@@ -15,7 +17,7 @@ public class BooksController : ControllerBase
         return Ok(books);
     }
 
-    [HttpGet("/{id}")]
+    [HttpGet("{id}")]
     public IActionResult GetBookById([FromRoute] int id)
     {
         var book = ApplicationContext.Books.Where(x => x.Id == id).FirstOrDefault();
@@ -32,7 +34,56 @@ public class BooksController : ControllerBase
         if (book is null) return BadRequest();
 
         ApplicationContext.Books.Add(book);
-        
+
         return StatusCode(201);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateBook([FromRoute] int id, [FromBody] Book bookModel)
+    {
+        Book? book = ApplicationContext.Books.Find(b => b.Id == id);
+        if (book is null) return NotFound();
+
+        book.Title = bookModel.Title;
+        book.Price = bookModel.Price;
+
+        return Ok(book);
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteAllBooks()
+    {
+        ApplicationContext.Books.Clear();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteBook([FromRoute] int id)
+    {
+        Book? book = ApplicationContext.Books.Find(x => x.Id == id);
+
+        if (book is null)
+        {
+            return NotFound(new
+            {
+                statusCode = 404,
+                message = $"Book with id:{id} not found!!"
+            });
+        }
+
+        ApplicationContext.Books.Remove(book);
+        return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public IActionResult PartiallyUpdateBook([FromRoute] int id, [FromBody] JsonPatchDocument<Book> bookPatch)
+    {
+        Book? book = ApplicationContext.Books.Find(x => x.Id == id);
+
+        if (book is null) return NotFound();
+
+        bookPatch.ApplyTo(book);
+
+        return NoContent();
     }
 }
