a59da4f7636d3680dba8c74695f9167deb3a3778
HBA114 <hbasriayhaner114@gmail.com>
2023-05-13 18:39:12 +0300

    Created IBookRepository and BookRepository

---
+++ bsStoreApp/Repositories/EFCore/BookRepository.cs
@@ -0,0 +1,23 @@
+using Entities.Models;
+
+using Repositories.Contracts;
+
+namespace Repositories.EFCore;
+
+public class BookRepository : RepositoryBase<Book>, IBookRepository
+{
+    public BookRepository(RepositoryContext context) : base(context)
+    {
+    }
+
+    public void CreateOneBook(Book book) => Create(book);
+
+    public void DeleteOneBook(Book book) => Delete(book);
+
+    public IQueryable<Book> GetAllBooks(bool trackChanges) => 
+        FindAll(trackChanges).OrderBy(b => b.Id);
+    public IQueryable<Book> GetBookById(int id, bool trackChanges) => 
+        FindByCondition(x => x.Id.Equals(id), trackChanges);
+
+    public void UpdateOneBook(Book book) => Update(book);
+}
