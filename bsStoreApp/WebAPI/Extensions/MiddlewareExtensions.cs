b04fd55762fc4ecf3b111bb56e208bc9d4a942c3
HBA114 <hbasriayhaner114@gmail.com>
2023-05-21 18:16:50 +0300

    Updated ConfigureExceptionHandler method

--- bsStoreApp/WebAPI/Extensions/MiddlewareExtensions.cs
+++ bsStoreApp/WebAPI/Extensions/MiddlewareExtensions.cs
@@ -1,6 +1,7 @@
 using System.Net;
 
 using Entities.ErrorModel;
+using Entities.Exceptions;
 
 using Microsoft.AspNetCore.Diagnostics;
 
@@ -16,18 +17,23 @@ public static class MiddlewareExtensions
         {
             appError.Run(async context =>
             {
-                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                 context.Response.ContentType = "application/json";
 
                 var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
 
                 if (contextFeature is not null)
                 {
+                    context.Response.StatusCode = contextFeature.Error switch
+                    {
+                        NotFoundException => StatusCodes.Status404NotFound,
+                        _ => StatusCodes.Status500InternalServerError
+                    };
+
                     loggerService.LogError($"Something went wrong: {contextFeature.Error}");
                     await context.Response.WriteAsync(new ErrorDetails()
                     {
                         StatusCode = context.Response.StatusCode,
-                        Message = "Internal Server Error"
+                        Message = contextFeature.Error.Message
                     }.ToString());
                 }
             });
