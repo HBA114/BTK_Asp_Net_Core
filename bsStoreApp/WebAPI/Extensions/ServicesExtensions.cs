70df22db56ed5299a734baa8a5cb0129800be08b
HBA114 <hbasriayhaner114@gmail.com>
2023-05-21 16:29:03 +0300

    Added nlog logger service, registered to services

--- bsStoreApp/WebAPI/Extensions/ServicesExtensions.cs
+++ bsStoreApp/WebAPI/Extensions/ServicesExtensions.cs
@@ -13,8 +13,13 @@ public static class ServicesExtensions
     public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
         services.AddDbContext<RepositoryContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("Default")));
+
     public static void ConfigureRepositoryManager(this IServiceCollection services) =>
         services.AddScoped<IRepositoryManager, RepositoryManager>();
-    public static void ConfigureServiceManager(this IServiceCollection services) => 
+
+    public static void ConfigureServiceManager(this IServiceCollection services) =>
         services.AddScoped<IServiceManager, ServiceManager>();
+
+    public static void ConfigureLoggerService(this IServiceCollection services) =>
+        services.AddSingleton<ILoggerService, LoggerManager>();
 }
