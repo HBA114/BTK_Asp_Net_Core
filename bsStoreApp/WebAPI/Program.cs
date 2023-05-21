02fb81948d1ab15ffece27ffe27c49c91ad4da24
HBA114 <hbasriayhaner114@gmail.com>
2023-05-21 17:17:56 +0300

    Configured global ExceptionHandler in Program.cs

--- bsStoreApp/WebAPI/Program.cs
+++ bsStoreApp/WebAPI/Program.cs
@@ -1,5 +1,7 @@
 using NLog;
 
+using Services.Contracts;
+
 using WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,11 +25,19 @@ builder.Services.ConfigureLoggerService();
 
 var app = builder.Build();
 
+var logger = app.Services.GetRequiredService<ILoggerService>();
+app.ConfigureExceptionHandler(logger);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+if (app.Environment.IsProduction())
+{
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
