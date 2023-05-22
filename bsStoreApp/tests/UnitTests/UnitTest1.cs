38f79286772f57187ff690b49f848864e5ab37de
HBA114 <hbasriayhaner114@gmail.com>
2023-05-22 11:14:39 +0300

    Added  MsTest project for unit testing, created task for debugging tests

---
+++ bsStoreApp/tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+
+namespace UnitTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Debug.WriteLine("Unit Test TestMethod1");
+    }
+}
