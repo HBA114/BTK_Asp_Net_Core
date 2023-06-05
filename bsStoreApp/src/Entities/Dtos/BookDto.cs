07939a0f3cc6297e5f90f6080b4606c044560368
HBA114 <hbasriayhaner114@gmail.com>
2023-06-05 16:37:33 +0300

    Updated dto for better xml output

--- bsStoreApp/src/Entities/Dtos/BookDto.cs
+++ bsStoreApp/src/Entities/Dtos/BookDto.cs
@@ -1,4 +1,8 @@
 namespace Entities.Dtos;
 
-[Serializable]
-public record BookDto(int Id, string Title, decimal Price);
+public record BookDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+}
